40a506f87ce5b96cfae26cefc0ee1ee3c0348cc1
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-03-19 14:20:16 -0300

    operadores: operador ternario

--- tipos-variaveis/src/edu/abel/app/Operadores.java
+++ tipos-variaveis/src/edu/abel/app/Operadores.java
@@ -2,37 +2,12 @@ package edu.abel.app;
 
 public class Operadores {
     public static void main(String[] args) {
-        int numero = 5;
+        int a, b;
+        a = 5;
+        b = 5;
 
-        numero = -numero;
+        String result = (a == b) ? "verdadeiro" : "falso";
 
-        System.out.println(- numero);
-        System.out.println(numero);
-
-        numero = -numero;
-        System.out.println(numero);
-
-        numero++;
-        System.out.println(numero);
-
-        numero--;
-        System.out.println(numero);
-
-        numero += 2;
-        System.out.println(numero);
-
-        System.out.println("regrinha incremento e decremento e bool");
-        System.out.println(numero --);
-        System.out.println(numero);
-
-        System.out.println(++ numero);
-        System.out.println(numero);
-
-        boolean euEVoce = true;
-        System.out.println(!euEVoce);
-        System.out.println(euEVoce);
-
-        euEVoce = !euEVoce;
-        System.out.println(euEVoce);
+        System.out.println(result);
     }
 }
