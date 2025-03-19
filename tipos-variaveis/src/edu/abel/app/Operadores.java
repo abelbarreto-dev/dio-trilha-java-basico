fd1522617c152c50950856916a3d25e6f2c17ab1
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-03-19 13:56:50 -0300

    operadores, concratenar strings e comportamento com numeros

---
+++ tipos-variaveis/src/edu/abel/app/Operadores.java
@@ -0,0 +1,20 @@
+package edu.abel.app;
+
+public class Operadores {
+    public static void main(String[] args) {
+        // concratenar strings
+        String nomeCompleto = "ABEL" + "CARVALHO";
+
+        String concatenacao = "?";
+
+        concatenacao = 1+1+1+"1";
+
+        concatenacao = 1 + "1" + 1 + 1;
+
+        concatenacao = 1 + "1" + 1 + "1";
+
+        concatenacao = "1" + 1 + 1 + 1;
+
+        concatenacao = "1" + (1+1+1);
+    }
+}
